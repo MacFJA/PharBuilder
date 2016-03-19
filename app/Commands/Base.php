ebb9f1e92f72204d1bee21081491e126a1420120
MacFJA <macfja@gmail.com>
2016-03-19 19:07:12 +0100

    Remove unused  statement

--- app/Commands/Base.php
+++ app/Commands/Base.php
@@ -12,7 +12,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\Question;
-use Symfony\Component\Finder\Finder;
 
 /**
  * Class Base.
