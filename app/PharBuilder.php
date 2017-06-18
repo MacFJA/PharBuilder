9ba2eff8b682dcd972d241c617deb5f5973221ec
Sergey <wapmorgan@gmail.com>
2017-06-18 22:31:52 +0300

    Update PharBuilder.php
    Fix for stub error on Unix/Linux when phar is built on Windows

--- app/PharBuilder.php
+++ app/PharBuilder.php
@@ -353,7 +353,7 @@ class PharBuilder
 
         $this->stubFile = $this->makePathRelative($this->stubFile);
         $this->phar->setStub(
-            (!$this->isSkipShebang() ? '#!/usr/bin/env php' . PHP_EOL : '') .
+            (!$this->isSkipShebang() ? '#!/usr/bin/env php' . "\n" : '') .
             '<?php Phar::mapPhar(); include "phar://' . $this->alias . '/' . $this->stubFile .
             '"; __HALT_COMPILER(); ?>'
         );
